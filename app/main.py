2837ef6b6e77c8b0a55c9d32d6ffbe9a558fd79a
Олег Халиков <o.halikov@ocas.ai>
2023-10-31 19:02:36 +0500

    init, add fastapi, uvicorn

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__": 
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
