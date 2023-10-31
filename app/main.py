4082af0bd30b8d5fed8122ed5f86079607ad856b
Олег Халиков <o.halikov@ocas.ai>
2023-10-31 20:16:52 +0500

    add logger, add main route

--- app/main.py
+++ app/main.py
@@ -1,13 +1,35 @@
-from typing import Union
+from typing import Union, List
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+from logging.config import dictConfig
+from logs.my_log_conf import log_config
+import logging
 
-app = FastAPI()
+dictConfig(log_config)
+
+class Todo(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
+
+app = FastAPI(debug=True)
+
+
+@app.get("/old")
+def read_root(todos: List[Todo] = None):
+    text_todo = [todo.text for todo in todos]
+    logger = logging.getLogger('foo-logger')
+    logger.debug('test')
+    return {"text_todo": 'dffffff'}
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    logger = logging.getLogger('foo-logger')
+    logger.debug('This is test 2')
+    return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
