523b8ecd161b5e275c8c86405d9c64798312eb84
Олег Халиков <o.halikov@ocas.ai>
2023-11-01 19:48:10 +0500

    add base CRUD to todo list

--- app/main.py
+++ app/main.py
@@ -1,43 +1,68 @@
 from typing import Union, List
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
-from logging.config import dictConfig
-from logs.my_log_conf import log_config
-import logging
-
-dictConfig(log_config)
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
 
 class Todo(BaseModel):
-    id: int
+    # id: UUID = Field(default_factory=uuid4)
     text: str
     completed: bool
 
 
 app = FastAPI(debug=True)
 
+#       [
+#         {
+#           "id": 0,
+#           "text": 'Learn the basics of Vue',
+#           "completed": True,
+#         },
+#         {
+#           "id": 1,
+#           "text": 'Learn the basics of Typescript',
+#           "completed": False,
+#         },
+#         {
+#           "id": 2,
+#           "text": 'Subscribe to the channel',
+#           "completed": False,
+#         },
+#         {
+#           "id": 3,
+#           "text": 'Learn the basics of JS',
+#           "completed": True,
+#         },
+#       ]
 
-@app.get("/old")
-def read_root(todos: List[Todo] = None):
-    text_todo = [todo.text for todo in todos]
-    logger = logging.getLogger('foo-logger')
-    logger.debug('test')
-    return {"text_todo": 'dffffff'}
+todos = {}
+
+@app.get("/todos")
+def read_todos():
+    return todos
 
 
-@app.get("/")
-def root():
-    logger = logging.getLogger('foo-logger')
-    logger.debug('This is test 2')
-    return {"message": "Hello World"}
+@app.post("/todos")
+def create_todo(todo: Todo):
+    todo_id = uuid4()
+    todos[todo_id] = todo
+    return todo
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: UUID, todo: Todo):
+    todos[todo_id] = todo
+    return todo
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: UUID):
+    todo = todos[todo_id]
+    del todos[todo_id]
+    return {'message': f"Item {{todo}} deleted"}
 
 
 if __name__ == "__main__": 
-    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    config = uvicorn.Config("main:app", port=5000, log_level="debug", reload=True)
     server = uvicorn.Server(config)
     server.run()
